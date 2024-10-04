ba1fb8992573908ea379d0575fa3a8f47fc149af
ArifahZhafirah <arifahzhafirah206@gmail.com>
2024-10-04 09:29:49 +0700

    Add files via upload

---
+++ Pertanyaan1Percobaan1.java
@@ -0,0 +1,48 @@
+// import java.util.Scanner;
+// /**
+//  * Pemilihan2Percobaan104
+//  */
+// public class Pemilihan2Percobaan104 {
+//     public static void main(String[] args) {
+//         Scanner input04 = new Scanner(System.in);
+    
+//         int tahun; 
+//         System.out.print("Masukkan tahun ");
+//         tahun = input04.nextInt();
+
+//         if ((tahun % 4) == 0 ) {
+//             if ((tahun % 100) != 0)
+//             System.out.println( "Tahun Kabisat ");
+//         } else 
+//             System.out.println(" Bukan Tahun Kabisat ");
+
+//     }
+    
+// }
+
+import java.util.Scanner;
+
+public class Pertanyaan1Percobaan1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int tahun;
+
+        System.out.println("Masukkan tahun:");
+        tahun = input.nextInt();
+
+        if (tahun % 4 == 0) {           
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) { 
+                    System.out.println("Tahun kabisat");
+                } else { 
+                    System.out.println("Bukan tahun kabisat");
+                }
+            } else { 
+                System.out.println("Tahun kabisat");
+            }
+        } else { 
+            System.out.println("Bukan tahun kabisat");
+        }
+    }
+}
